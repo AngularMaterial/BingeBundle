81a163ee2f14842ba6bde0062836fe5ab4ec8e4d
arwas11 <146148342+arwas11@users.noreply.github.com>
2024-08-06 14:09:39 -0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiKey :'c347zU1xGaZw7K0zHTlUqKmlIZjvFV1qywk9l9C7',
-    bearerToken: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTExYTBhZDNjN2Y3NGE1ODY3MThkODRlYzdjMTNlNiIsInN1YiI6IjY2MTk1ODA2YWYzZGE2MDE2MzE4OTM1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bEGEsfW6lpxXIlOoiGs616XICs0SfBIZlBSXvvVAR1w',
+    apiKey :'501a0e87171c0ed8655a198c22a4ebab',
+    bearerToken: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MDFhMGU4NzE3MWMwZWQ4NjU1YTE5OGMyMmE0ZWJhYiIsIm5iZiI6MTcyMjk3MTI1MS4xNzkzNzksInN1YiI6IjY2MTk1ODA2YWYzZGE2MDE2MzE4OTM1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.W2JSjA6NikX0wIvHGVc19T_QJMKssJcTp1rekGO7Z5o',
     baseUrl: 'https://api.themoviedb.org/3'
 };
